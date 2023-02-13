404739df32b052ed9de11b7cb86d95e719983ba8
allesoares95 <allesoares_@live.com>
2023-02-13 18:03:04 -0300

    component change part 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Sparkle } from 'phosphor-react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 import './global.css';
 import { Tweet } from './components/Tweet';
 import { Sidebar } from './components/Sidebar';
+import { Header } from './components/Header';
+import { Separator } from './components/Separator';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -13,10 +14,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       
       <div className="content">
         <main className="timeline">
-          <div className="timeline-header">
-            Home
-            <Sparkle />
-          </div>
+          <Header title="Home" />
 
           <form className="new-tweet-form">
             <label htmlFor="tweet">
@@ -32,7 +30,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 
           </form>
 
-          <div className="separator" />
+          <Separator />
 
           <Tweet />
           <Tweet />
